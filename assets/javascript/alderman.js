181250fe58a4b97180c7e1e378b819250600402c
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-10-20 15:08:01 -0500

    maplace map replaces googlemap

--- assets/javascript/alderman.js
+++ assets/javascript/alderman.js
@@ -1,51 +1,56 @@
-var wardOffices = [
-    {
-        lat: 41.919350,
-        lon: -87.687980,
-        title: 'Title A1',
-        html: '<h3>Content A1</h3>',
-        icon: 'http://maps.google.com/mapfiles/markerA.png',
-        // animation: google.maps.Animation.DROP
-    }];
+$(function () {
 
-$.ajax({
-    url: "https://data.cityofchicago.org/resource/7ia9-ayc2.json",
-    type: "GET",
-    data: {
-        "$limit": 5000,
-        "$$app_token": "K7i1lV0vqt5nRSZfFySmhy84t"
-    }
-}).done(function (data) {
-    console.log("data loaded");
+    var wardOffices = [
+        {
+            lat: 41.919350,
+            lon: -87.687980,
+            title: 'Ward1',
+            html: '<h3>Ward 1</h3>',
+            icon: 'http://maps.google.com/mapfiles/markerA.png',
+            // animation: google.maps.Animation.DROP,
+        }, {
+            lat: 41.907080,
+            lon: -87.667810,
+            title: 'Ward2',
+            html: '<h3>Ward 1</h3>',
+            icon: 'http://maps.google.com/mapfiles/markerA.png',
+            // animation: google.maps.Animation.DROP,
+        }, {
+            lat: 41.801880,
+            lon: -87.652230,
+            title: 'Ward3',
+            html: '<h3>Ward 1</h3>',
+            icon: 'http://maps.google.com/mapfiles/markerA.png',
+            // animation: google.maps.Animation.DROP,
+        }];
 
-});
+    new Maplace({
+        show_markers: false,
+        locations: [{
+            lat: 41.881832,
+            lon: -87.623177,
+            zoom: 12
+        }]
+    }).Load();
 
-// Initialize and add the map
-function initMap() {
-    // The location of Chicago
-    var chicago = { lat: 41.881832, lng: -87.623177 };
-    // The map, centered at Chicago
-    var map = new google.maps.Map(
-        document.getElementById('gmap'), { zoom: 11, center: chicago });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({ position: chicago, map: map });
-}
-
-//dropdown example
-new Maplace({
-    locations: wardOffices,
-    map_div: '#map-dropdown',
-    controls_title: 'Choose a location:',
-    listeners: {
-        click: function (map, event) {
-            alert('That was a click!');
+    // dropdown example
+    new Maplace({
+        locations: wardOffices,
+        map_div: '#gmap-dropdown',
+        controls_title: 'Choose a location:',
+        listeners: {
+            click: function (map, event) {
+                alert('That was a click!');
+            }
         }
-    }
-}).Load();
+    }).Load();
 
-var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(entry.location_1.latitude,
-        entry.location_1.longitude),
-    map: map,
-    title: location.name
+
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(entry.location_1.latitude,
+            entry.location_1.longitude),
+        map: map,
+        title: location.name
+    })
 });
+
