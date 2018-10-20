6662ad5c9e2bd3bf93d481dba60667dd880a79f0
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-10-20 00:09:13 -0500

    working button

--- assets/javascript/alderman.js
+++ assets/javascript/alderman.js
@@ -2,7 +2,14 @@ $(document).ready(function () {
 
 
 
+    //handles the button click event
+    $("#search-btn").on("click", function (event) {
+        event.preventDefault();
+        console.log("woot woot!")
+        var userInput = $("#user-input").val().trim();
 
 
+    })
 
-}
+
+});
