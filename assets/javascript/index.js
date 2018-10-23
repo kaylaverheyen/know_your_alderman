b719b0fdc88c952f3077e8867c0a851e21d63893
Balerina <katarzynab9236@gmail.com>
2018-10-23 18:29:36 -0500

    image change

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -6,21 +6,26 @@ $(document).ready(function () {
 
     // On click event that displays info card for each star button pressed
     $("#btn1").on("click", function () {
+        $("#btn1").addClass("animated pulse");
         $("#card1").toggle();
         $("#card1").addClass("animated flipInX");
+
     });
 
     $("#btn2").on("click", function () {
+        $("#btn2").addClass("animated pulse");
         $("#card2").toggle();
         $("#card2").addClass("animated flipInX");
     });
 
     $("#btn3").on("click", function () {
+        $("#btn3").addClass("animated pulse");
         $("#card3").toggle();
         $("#card3").addClass("animated flipInX");
     });
 
     $("#btn4").on("click", function () {
+        $("#btn4").addClass("animated pulse");
         $("#card4").toggle();
         $("#card4").addClass("animated flipInX");
     });
